c6b0cb3c0e82f3362bfc66143fe2c59b3a998e36
neverclear <neverclear86sab@gmail.com>
2019-03-28 22:37:47 +0900

    コンポーネント追加

---
+++ front/src/components/Login.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .login
+    #firebaseui-auth
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator'
+  import firebaseui from 'firebaseui'
+
+  @Component
+  export default class Login extends Vue {
+    private mounted() {
+      const ui = new firebaseui.auth.AuthUI(firebase.auth())
+      ui.start('firebaseui-auth', {
+        signInSuccessUrl: '',
+      })
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
---
+++ front/src/components/Register.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .register
+    v-layout(row wrap)
+      h2 グループ登録
+    v-flex(x10)
+      v-input
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator'
+
+  @Component
+  export default class Register extends Vue {
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
